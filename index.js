95aef054196552f0f4bec85443f2d443fc7a2a83
Carmen Regina <crcarmen23@gmail.com>
2023-10-12 13:23:05 -0500

    edited questions, added init function

--- index.js
+++ index.js
@@ -1,51 +1,101 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+const generateReadme = ({projectTitle, description, installation, usage, credits, license, tests, contributing})
+
 const questions = [
     {type: 'input',
     message: 'What is your Github username?',
-    name: 'Username'
+    name: 'username'
 }, {
     type: 'input',
     message: 'What is your email address?',
-    name: 'Email'
+    name: 'email'
 }, {
     type: 'input',
-    message: 'What is your projects name?',
-    name: 'Project Title'
+    message: 'What is the title of your project?',
+    name: 'projectTitle'
 }, {
     type: 'input',
-    message: 'Please write a description for your project',
-    name: 'Description'
+    message: 'Please write a short description explaining the what, why, and how of your project.',
+    name: 'description'
+}, {
+    type: 'input',
+    message: 'What are the steps required to install your project?',
+    name: 'installation'
+}, {
+    type: 'input',
+    message: 'Provide instructions and examples for use.',
+    name: 'usage'
+}, {
+    type: 'input',
+    message: 'Who did you collaborate with on this project?',
+    name: 'credits'
 }, {
     type: 'input',
     message: 'What kind of License do you want your project to have?',
-    name: 'License'
-}, {
-    type: 'input',
-    message: 'What command should be run to install dependencies?',
-    name: 'Installation'
-}, {
-    type: 'input',
-    message: 'What command should be run to run tests?',
-    name: 'Tests'
-}, {
-    type: 'input',
-    message: 'What should the user know about the usage for this repo?',
-    name: 'Usage'
+    name: 'license',
 }, {
     type: 'input',
     message: 'What should the user know about contributing to the repo?',
-    name: 'Contributing'
+    name: 'contributing'
+}, {
+    type: 'input',
+    message: 'What command should be run to run tests?',
+    name: 'tests'
 }
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+function generateMarkdown(data) {
+    return `
+# ${data.title}
 
-// Function call to initialize app
+## Description
+    
+${data.description}
+    
+## Table of Contents
+       
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+    
+## Installation
+    
+${data.installation}
+    
+## Usage
+    
+${data.usage}
+    
+## Credits
+    
+${data.credits}
+    
+## License
+    
+${data.license}
+    
+## How to Contribute
+    
+Collaborated with ${data.contributing}.
+    
+## Tests
+
+${data.tests}
+  `;
+  }
+
+function init() {
+inquirer.prompt(questions)
+.then((data) => {
+    const readmeContent = generateReadme(data);
+    
+    fs.writeFile('README.md', JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+  );
+}); }
+
 init();
